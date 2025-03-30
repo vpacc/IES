dcb90114bd591beb0e016c02c574cd2e3c0c7fd1
vb <bac654765@gmail.com>
2025-03-31 01:01:27 +0700

    user updated

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res) => {
         case "user.created":{
             const userData = {
                 _id: data.id,
-                email: data.email_address[0].email_address,
+                email: data.email_addresses[0].email_address,
                 name: data.first_name + " " + data.last_name,
                 imageUrl: data.image_url,
             }
@@ -47,7 +47,6 @@ export const clerkWebhooks = async (req, res) => {
         default:
             break;
      }
-     res.status(200).send("Webhook processed successfully")
   } catch(error) {
        res.json({success: false, message: error.message})
   }

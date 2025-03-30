ca290bf8f130938aa5fb84cbb61ba55eaa1cd2cd
vb <bac654765@gmail.com>
2025-03-31 00:16:55 +0700

    124

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
  const connectDB = async () => {
-    mongoose.connection.on('connected', ()=> console.log('Database connected')) 
+    mongoose.connection.on('connected', ()=> console.log('Database Connected')) 
 
     await mongoose.connect(`${process.env.MONGODB_URI}/IES`)
  }
 
- export default connectDB;
+ export default connectDB
